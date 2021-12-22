742a4393a4d30006750def629058946ac33323a3
ridprivates@gmail.com <ridprivates@gmail.com>
2021-12-22 09:08:37 +0800

    u

--- plugins/file_cabinet/ui.py
+++ plugins/file_cabinet/ui.py
@@ -1,7 +1,13 @@
+from PySide6.QtWidgets import QVBoxLayout
 from .tool import tool
 
-from upd.ui import MainPanel
+from upd.ui import MainPanel, RLineEdit, RGridView
 
 @tool.main_panel
 class FileCabinetPanel(MainPanel):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        self.grid = RGridView()
+        layout.addWidget(self.grid)
